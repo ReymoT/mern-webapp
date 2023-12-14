3cf3f956eaad265b14d629f5aaf4ec28c0a6196f
ReymoT <tamme.reimo@gmail.com>
2023-12-14 14:38:21 -0700

    connected to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB!");
+}).catch(() => {
+    console.log("Could not connect to MongoDB");
+});
 
 const app = express();
 
