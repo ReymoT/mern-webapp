4cde1b8e093028bbef91196fc15aac31906c036a
ReymoT <tamme.reimo@gmail.com>
2023-12-15 20:53:57 -0700

    create sign up API route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,19 +4,19 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
     }
 }, { timestamps: true });
 
-const User = mongoose.Model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
